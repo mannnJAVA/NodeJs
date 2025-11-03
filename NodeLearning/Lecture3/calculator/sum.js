ff3511bb5fba4c4bec80562ec69d566623a1d1e6
mannnJAVA <mannn.html@gmail.com>
2025-11-03 16:12:34 +0530

    practice set

---
+++ NodeLearning/Lecture3/calculator/sum.js
@@ -0,0 +1,28 @@
+const { URLSearchParams } = require("node:url");
+
+const sumRequestHandler = (req, res) => {
+  console.log("In sum request Handler ", req.url);
+  const body = [];
+  req.on("data", (chunk) => {
+    body.push(chunk);
+    console.log(body);
+  });
+  req.on("end", () => {
+    const bodyStr = Buffer.concat(body).toString();
+
+    const params = new URLSearchParams(bodyStr);
+    const bodyObj = Object.fromEntries(params);
+    const result = Number(bodyObj.first) + Number(bodyObj.second);
+    console.log(result);
+    res.setHeader("Content-type", "text/html");
+    res.write(`<html>
+      <head><title>Practice set</title></head>
+      <body>
+      <h1>Your sum is ${result}</h1>
+      </body>
+      </html>`);
+    return res.end();
+  });
+};
+
+exports.sumRequestHandler = sumRequestHandler;
