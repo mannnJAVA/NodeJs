dbd557430dd58330e72152263fd0dd2c1c2003aa
mannnJAVA <mannn.html@gmail.com>
2025-11-05 19:05:25 +0530

    Express Routers and adding 404 not found

--- LearningExpress/Airbnb/app.js
+++ LearningExpress/Airbnb/app.js
@@ -1,14 +1,25 @@
 const express = require("express"); // external module
 
+const userRouter = require("./routes/userRouter"); //Local module
+const hostRouter = require("./routes/hostRouter");
+
 const app = express();
 
 app.use((req, res, next) => {
   console.log(req.url, req.method);
-  res.send("<h1>Welcome to airbnb</h1>");
+  next();
+});
+
+app.use(express.urlencoded());
+app.use(userRouter);
+app.use(hostRouter);
+
+app.use((req, res, next) => {
+  res.status(404).send("<h1>404 Page Not Found</h1>");
 });
 
 const PORT = 7000;
 
 app.listen(PORT, () => {
-  console.log(`Server is runnign on http://localhost${PORT}`);
+  console.log(`Server is runnign on http://localhost:${PORT}`);
 });
